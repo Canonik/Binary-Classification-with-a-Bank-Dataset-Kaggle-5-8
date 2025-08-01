d55b7fb2f7956ec4153fda578b758acda317292b
Canonik <alessandro006@icloud.com>
2025-08-01 16:00:14 +0200

    Added a prediction sanity pipeline and trained model

--- src/data_loading.py
+++ src/data_loading.py
@@ -4,10 +4,11 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 def standard_training_set():
-    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/train.csv")
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/train.csv").set_index("id")
 
 def standard_test_set():
-    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/test.csv")
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/test.csv").set_index("id")
 
 def full_original_database():
-    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/bank-full.csv")
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/bank-full.csv").set_index("id")
+
