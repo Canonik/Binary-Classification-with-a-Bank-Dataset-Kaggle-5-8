430f0c1c415a34199ade9c23efaddc7cebd2f3f6
Canonik <alessandro006@icloud.com>
2025-08-01 12:18:50 +0200

    Initial commit

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 def standard_training_set():
-    return pd.read_csv(Path("data/playground-series-s5e8/train.csv"))
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/train.csv")
 
 def standard_test_set():
-    return pd.read_csv(Path("data/playground-series-s5e8/test.csv"))
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/test.csv")
 
 def full_original_database():
-        return pd.read_csv(Path("data/playground-series-s5e8/bank-full.csv"))
+    return pd.read_csv(PROJECT_ROOT / "data/playground-series-s5e8/bank-full.csv")
