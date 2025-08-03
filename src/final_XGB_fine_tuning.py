380c99718bb7acfacef20100e71c2d42eca8d056
Canonik <alessandro006@icloud.com>
2025-08-03 16:23:58 +0200

    added KNN pruning

--- src/final_XGB_fine_tuning.py
+++ src/final_XGB_fine_tuning.py
@@ -8,7 +8,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
 from xgboost import XGBClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint, uniform
@@ -21,12 +21,13 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
 from data_loading import standard_training_set, standard_test_set, full_original_database
-from pipelines import day_month_encoding, log_encoding, LogTransformer, DayOfYearTransformer
+from pipelines import day_month_encoding, log_encoding, LogTransformer, DayOfYearTransformer#, KNN_useless_features
 
 num_att = ["age", "pdays", "previous"]
 cat_att =["job", "marital", "education", "default", "housing", "loan", "contact", "poutcome"]
 log_att =["balance", "duration", "campaign"]
 time_att = ["day", "month"]
+#useless_att = []
 
 clients = standard_training_set().reset_index(drop=True)
 clients_attr = clients.drop("y", axis=1)
@@ -57,10 +58,16 @@ day_pipeline = make_pipeline(
     StandardScaler()
 )
 
+#useless_oof_pipeline = make_pipeline(
+    #KNN_useless_features(),
+    #StandardScaler()
+#)
+
 preprocessing = ColumnTransformer([
     ("num", num_pipeline, num_att),
     ("cat", cat_pipeline, cat_att),
     ("log", log_pipeline, log_att),
+    #("KNN_oof", useless_oof_pipeline, useless_att)
 ])
 preprocessing_doy = ColumnTransformer([
     ("day", day_pipeline, time_att)
@@ -82,24 +89,24 @@ def K_fold_estimation():
 def Total_dataset_training():
     
     best_params = {
-    'colsample_bytree': 0.5099085529881076,
-    'gamma': 0.28242799368681437,
-    'learning_rate': 0.025703519227860273,
-    'max_depth': 5,
-    'min_child_weight': 6,
-    'n_estimators': 1366,
-    'reg_alpha': 9.833985769487203,
-    'reg_lambda': 5.200866039231819,
-    'subsample': 0.6579821220208961
+    'colsample_bytree': 0.63,
+    'gamma': 1.09,
+    'learning_rate': 0.014,
+    'max_depth': 11,
+    'min_child_weight': 7,
+    'n_estimators': 2995,
+    'reg_alpha': 4.14,
+    'reg_lambda': 12.06,
+    'subsample': 0.83
     }
     XGB_model = make_pipeline(full_feature_pipeline, XGBClassifier(use_label_encoder=False, eval_metric="logloss", random_state=42, **best_params))
     XGB_model.fit(clients_attr, clients_labels)
-    joblib.dump(XGB_model, "models/XGB_finetuned1.pkl")
+    joblib.dump(XGB_model, "models/XGB_finetuned_15h.pkl")
 
     predictions = XGB_model.predict_proba(clients_test)
     predictions_df = pd.DataFrame( predictions[ :, 1], columns=["y"], index= clients_test.index)
 
-    predictions_df.to_csv("reports/XGB_finetuned1.csv")
+    predictions_df.to_csv("reports/XGB_finetuned_15h.csv")
 
 def Fine_tuning_pipeline():
     parameters = {
@@ -115,7 +122,7 @@ def Fine_tuning_pipeline():
     }
 
     XGB_model = make_pipeline(full_feature_pipeline, XGBClassifier(use_label_encoder=False, eval_metric="logloss", random_state=42))
-    rnd_search = RandomizedSearchCV(XGB_model, parameters, n_iter=300, cv=5, scoring="roc_auc", random_state = 42, verbose=3, n_jobs=6)
+    rnd_search = RandomizedSearchCV(XGB_model, parameters, n_iter=300, cv=5, scoring="roc_auc", random_state = 42, verbose=3)
     rnd_search.fit(clients_attr, clients_labels)
 
     print(f"Best ROC AUC: {rnd_search.best_score_:.5f}")
@@ -141,4 +148,4 @@ def Fine_tuning_pipeline():
     pd.DataFrame([rnd_search.best_params_]).to_csv("reports/model_parameters_final_XGB_fine_tuning.csv")
 
 if __name__ == "__main__":
-    Fine_tuning_pipeline()
+    Total_dataset_training()
--- src/pipelines.py
+++ src/pipelines.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsRegressor
 from pathlib import Path
 import datetime
 
@@ -95,4 +98,46 @@ class DayOfYearTransformer(BaseEstimator, TransformerMixin):
         return np.array(["day_of_the_year"])
 
 
+class KNN_useless_features(BaseEstimator, TransformerMixin):
+    def __init__(self, features, n_neighbors=5, n_splits=5, random_state=42):
+        self.features = features 
+        self.n_neighbors = n_neighbors
+        self.n_splits = n_splits
+        self.random_state = random_state
 
+    def fit(self, X, y):
+        X_weak = X[self.features].copy()
+
+        self.knn_models = []
+        self.oof_predictions = np.zeros(len(X_weak.index))
+        self.imputer = SimpleImputer(strategy="mean")
+        X_weak = self.imputer.fit_transform(X_weak)
+        
+        skf = KFold(n_splits = self.n_splits, random_state=self.random_state, shuffle=True)
+        
+        for train_index, test_index in skf.split(X_weak, y):
+            X_weak_train = X_weak[train_index]
+            Y_train = y[train_index]
+
+            X_weak_test = X_weak[test_index]
+
+            k_ngb = KNeighborsRegressor(n_neighbors=self.n_neighbors, weights="distance")
+            k_ngb = k_ngb.fit(X_weak_train, Y_train)
+            self.oof_predictions[test_index] = k_ngb.predict(X_weak_test)
+            self.knn_models.append(k_ngb)
+
+        return self
+    
+    def transform(self, X):
+        X_wk = X[self.features].copy()
+        X_wk = self.imputer.transform(X_wk)
+
+        all_pred = np.zeros(shape=(X_wk.shape[0], self.n_splits))
+
+        for i, model in enumerate(self.knn_models):
+            all_pred[:, i] = model.predict(X_wk)
+
+        return np.mean(all_pred, axis=1).reshape(X_wk.shape[0],1)
+    
+    def get_feature_names_out(self):
+        return [f"knn_oof_mean__{'_'.join(self.features)}"]
